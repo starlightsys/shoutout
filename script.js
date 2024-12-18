6c5605ab1977494b3e9fb6db145407d4e89b240e
Starlight Galaxy <github@starlight.lgbt>
2024-12-18 18:24:11 +0100

    Add textFit

--- script.js
+++ script.js
@@ -62,6 +62,7 @@ function generatePreview() {
       supporters.appendChild(nameElement);
     });
   }
+  textFit(document.getElementsByClassName('name'), { maxFontSize: 60 });
 }
 
 function downloadImage() {
